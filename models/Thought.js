76e287661d8a5ff46a7de0ec7cd3cb9860c336db
Andrew Biron <andrewbiron@andrews-air.home>
2024-03-21 14:57:26 -0400

    completed the Thought model by including reactionSchema to it

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,29 @@
 const { Schema, model } = require('mongoose');
+const { monitorEventLoopDelay } = require('perf_hooks');
 
+//Define reactionSchema as a sub-document
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: mongoose.Types.ObjectId,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+//Define thoughtSchema 
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -12,6 +36,11 @@ const thoughtSchema = new Schema(
             type: Date,
             default: Date.now,
         },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
@@ -21,8 +50,19 @@ const thoughtSchema = new Schema(
     }
 );
 
+//getter format for reaction
+reactionSchema.virtual('formattedCreatedAt').get(function (){
+    return this.createdAt.toLocaleString();
+});
+
+////getter format for thought
 thoughtSchema.virtual('formattedCreatedAt').get(function (){
     return this.createdAt.toLocaleString();
+});
+
+//Counting the reactions
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
 });
 
 const Thought = model('thought', thoughtSchema);
